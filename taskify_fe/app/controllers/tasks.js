e2c8a3986e749c4241400e2ff2b2376dfbf3fefd
Alexander Brade <alex.brade@gmx.de>
2018-11-01 20:17:05 +1300

    taskify_fe: Add table-grid component and a tasks controller

---
+++ taskify_fe/app/controllers/tasks.js
@@ -0,0 +1,69 @@
+import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+
+export default Controller.extend({
+  queryParams: ['page', 'limit', 'state'],
+  page: 1,
+  limit: 20,
+  next: computed(
+    'model.meta.pagination.{self,next}.page',
+    function() {
+      const isNext = this.get('model.meta.pagination.self.page') != this.get('model.meta.pagination.next.page');
+      console.log("NEXT", isNext, this.get('model.meta.pagination.self.page'), this.get('model.meta.pagination.next.page'));
+      return isNext;
+    }
+  ),
+  prev: computed(
+    'model.meta.pagination.{prev,self}.page',
+    function() {
+      const isPrev = this.get('model.meta.pagination.self.page') != this.get('model.meta.pagination.prev.page');
+      console.log("PREV", isPrev, this.get('model.meta.pagination.self.page'), this.get('model.meta.pagination.prev.page'));
+      return isPrev;
+    }
+  ),
+  columns: computed(function() {
+    return [
+      {
+        name: 'ID',
+        column: 'id',
+        hasLabel: true,
+        columnLabel: 'taskColor'
+      },
+      {
+        name: 'Title',
+        column: 'title',
+        hasLabel: false
+      },
+      {
+        name: 'Script',
+        column: 'script.name',
+        hasLabel: false
+      },
+      {
+        name: 'Worker',
+        column: 'worker.name',
+        hasLabel: false
+      },
+      {
+        name: 'Scheduled',
+        column: 'scheduled',
+        hasLabel: false
+      },
+      {
+        name: 'Run',
+        column: 'run',
+        hasLabel: false
+      },
+      {
+        name: 'Team',
+        column: 'script.team.name',
+        hasLabel: false
+      },
+      {
+        name: 'State',
+        column: 'state',
+        hasLabel: false
+      },
+    ]
+  })
+});
